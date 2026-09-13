e3cdf83bee608099ad3ffc00865721beeaaf2ff6
Uwakmfon Moses Ekong <uwakmfonmosesekong@Uwakmfons-MacBook-Pro.local>
2026-09-13 19:17:06 +0100

    google indexing and updating og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "LeBold Studios",
-  description: "Professional photography studio since 2008.",
+  verification: {
+    google: "XiqH1097vsfqZv7yWV3fMWCX2nJLN1FNwABWDxlJW0k",
+  },
+
   icons: {
     icon: [
       { url: "/logo.png", sizes: "32x32", type: "image/png" },
@@ -23,11 +26,11 @@ export const metadata: Metadata = {
   openGraph: {
     title: "LeBold Studios",
     description: "Professional photography studio since 2008.",
-    url: "https://www.leboldstudios.com",
+    url: "https://leboldstudios.com",
     siteName: "LeBold Studios",
     images: [
       {
-        url: "https://www.leboldstudios.com/og-image.jpg",
+        url: "https://leboldstudios.com/og-image.jpg",
         width: 1200,
         height: 630,
         alt: "LeBold Studios",
@@ -39,6 +42,6 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "LeBold Studios",
     description: "Professional photography studio since 2008.",
-    images: ["https://www.leboldstudios.com/og-image.jpg"],
+    images: ["https://leboldstudios.com/og-image.jpg"],
   },
 };
